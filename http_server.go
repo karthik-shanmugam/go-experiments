7c3eb91e0bb2b7b95ff430136fe8fc6a92cd3137
Karthik Shanmugam <karthiks@berkeley.edu>
2016-12-21 00:07:36 -0800

    initial commit

---
+++ http_server.go
@@ -0,0 +1,149 @@
+package main
+
+import "fmt"
+import "html/template"
+import "net/http"
+// import "math/rand"
+import "database/sql"
+import "log"
+import "time"
+import _ "github.com/mattn/go-sqlite3"
+
+type post struct {
+    id int
+    name string
+    content string
+    timestamp int64
+}
+
+type Template_Post struct {
+    Name string
+    Content string
+    Timestamp string
+}
+
+func (p *post) Template() Template_Post {
+    return Template_Post{p.name, p.content, time.Unix(p.timestamp, 0).Format(time.UnixDate)}
+}
+
+func (p *post) String() string {
+    return fmt.Sprintf("post(%d, \"%s\", \"%s\", %d)", p.id, p.name, p.content, p.timestamp)
+}
+
+func main() {
+
+    db := openDb("./posts.db")
+    defer db.Close()
+    initializePostsTable(db)
+    //p := &post{name: "karthik", content: "hello world", timestamp: time.Now().Unix()}
+    // addPost(db, p)
+
+    t, _ := template.ParseFiles("templates/posts.html")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // w.Write([]byte(fmt.Sprintf("You learned Go in %d minutes!\n", rand.Intn(100))))
+        fmt.Println("Handling an http thingy I guess")
+        if r.Method == "GET" {
+            showPosts(w, r, db, t)
+        } else if r.Method == "POST" {
+            makePost(w, r, db)
+            redirect_home(w, r)
+        } else {
+            fmt.Fprintf(w, "Invalid HTTP method!\n")
+        }
+        // posts := getPosts(db)
+        // template_posts := make([]Template_Post, 0)
+        // for _, p := range posts {
+        //     template_posts = append(template_posts, p.Template())
+        // }
+        // t.Execute(w, template_posts)
+        // for _, p := range posts {
+        //     fmt.Println(p.String())
+        // }
+    })
+    
+    err := http.ListenAndServe(":8080", nil)
+    fmt.Println(err)
+
+}
+func redirect_home(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/", 303)
+}
+func showPosts(w http.ResponseWriter, r *http.Request, db *sql.DB, t *template.Template) {
+    posts := getPosts(db)
+    template_posts := make([]Template_Post, 0)
+    for _, p := range posts {
+        template_posts = append(template_posts, p.Template())
+    }
+    t.Execute(w, template_posts)   
+}
+func makePost(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+    name := r.FormValue("name")
+    content := r.FormValue("content")
+    if len(name) > 0 && len(content) > 0 {
+        addPost(db, &post{name: name, content: content, timestamp: time.Now().Unix()})
+    }
+}
+
+
+
+func openDb(filename string) (*sql.DB) {
+    db, err := sql.Open("sqlite3", filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return db
+}
+
+func initializePostsTable(db *sql.DB) {
+    sqlStmt := `
+    create table if not exists posts (id integer not null primary key, name text, content text, time datetime);
+    `
+    _, err := db.Exec(sqlStmt)
+    if err != nil {
+        log.Printf("%q: %s\n", err, sqlStmt)
+        return
+    }
+}
+
+func addPost(db *sql.DB, p *post) {
+    tx, err := db.Begin()
+    if err != nil {
+        log.Fatal(err)
+    }
+    stmt, err := tx.Prepare("insert into posts (name, content, time) values(?, ?, ?)")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer stmt.Close()
+    _, err = stmt.Exec(p.name, p.content, p.timestamp)
+    if err != nil {
+        log.Fatal(err)
+    }
+    tx.Commit()
+}
+
+func getPosts(db *sql.DB) ([]post){
+    rows, err := db.Query("select id, name, content, time from posts")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    posts := make([]post, 0)
+    for rows.Next() {
+        var p post
+        var t time.Time
+        err = rows.Scan(&p.id, &p.name, &p.content, &t)
+        p.timestamp = t.Unix()
+        if err != nil {
+            log.Fatal(err)
+        }
+        posts = append(posts, p)
+    }
+    err = rows.Err()
+    if err != nil {
+        log.Fatal(err)
+    }
+    return posts
+}
